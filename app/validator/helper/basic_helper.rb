e7b79a02196418f8d02ab80298e1d13076885310
Benjie Tigley <benjiebtigley@gmail.com>
2022-08-07 19:51:04 +0800

    Added interactor and validator.

---
+++ app/validator/helper/basic_helper.rb
@@ -0,0 +1,14 @@
+module Helper
+  module BasicHelper
+    PASSWORD_REQUIREMENTS = 'Password should have more than 6 characters including 1 lower letter, 1 uppercase letter, 1 number and 1 symbol'
+    EMAIL_EXISTS = 'Email address already exist.'
+
+    def valid_date?(value)
+      return true if value == 'never'
+
+      !(value.match(/\d{4}-\d{2}-\d{2}/) && Time.zone.strptime(value, '%Y-%m-%d')).nil?
+    rescue ArgumentError
+      false
+    end
+  end
+end
